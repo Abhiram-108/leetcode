517dc95ae15fc889b38757415e5550ca0b19990d
Abhiram-108 <149411236+Abhiram-108@users.noreply.github.com>
2024-05-04 12:28:33 +0530

    Time: 59 ms (47.36%), Space: 45.3 MB (97.56%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>&a, int limit) {
-        sort(a.begin(),a.end());
+        sort(a.begin(),a.end());//sort the given people array
         int ct=0;
         int left=0;
         int right=a.size()-1;
         while(left<=right){
-            if(a[left]+a[right]<=limit){
+            if(a[left]+a[right]<=limit){ //if left person and right person are to be fitted in boat then we will increase left++ and right--;
+                //else we will place right person only in boat
                 left++;
             }
             right--;
