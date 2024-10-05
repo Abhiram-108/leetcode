7ed7a742402ae936cc34fd8f4118532595ba64b2
Abhiram Pedada <149411236+Abhiram-108@users.noreply.github.com>
2024-10-05 16:00:33 +0530

    Time: 0 ms (100.00%), Space: 14 MB (53.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,36 @@
 class Solution {
-    int binarysearch(vector<int>&nums,int low,int high,int target){
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target)
-                return mid;
-            if(nums[mid]>target)
-                high=mid-1;
-            if(nums[mid]<target)
-                low=mid+1;
-                
+    int binarysearch(vector<int>&nums,int left,int right,int target){
+       while (left <= right) 
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
+
+            if (nums[left] <= nums[mid]) 
+            {
+                if (target >= nums[left] && target < nums[mid]) right = mid - 1;
+                else left = mid + 1;
+            }
+            else 
+            {
+                if (target > nums[mid] && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
+            }
         }
+
         return -1;
     }
 public:
     int search(vector<int>& nums, int target) {
-       //  int n=nums.size();
-       //  int ans=binarysearch(nums,0,n-1,target);
-       // return ans;
-        int ind=0;
-        for(auto it:nums){
-            if(it==target)
-                return ind;
-            ind++;
-        }
-        return -1;
+        int n=nums.size();
+        int ans=binarysearch(nums,0,n-1,target);
+       return ans;
+        // int ind=0;
+        // for(auto it:nums){
+        //     if(it==target)
+        //         return ind;
+        //     ind++;
+        // }
+        // return -1;
         
     }
 };
