fe2a57bffdcfbcbe2f64ecbe3d346f06632288e3
Abhiram Pedada <149411236+Abhiram-108@users.noreply.github.com>
2025-07-12 11:21:09 +0530

    Time: 345 ms (56.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select e.name 
+from Employee e
+where e.id in
+(select e.managerId 
+from Employee e group by e.managerId
+having count(DISTINCT e.id)>=5
+
+
+);
