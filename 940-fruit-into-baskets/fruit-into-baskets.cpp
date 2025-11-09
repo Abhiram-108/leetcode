cd5b3009a63d772454535214f534088199820e6c
Abhiram Pedada <149411236+Abhiram-108@users.noreply.github.com>
2025-11-09 19:47:46 +0530

    Time: 42 ms (49.99%) | Memory: 84.9 MB (6.03%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        //it a 2 pointer technique and sliding window problem
+       unordered_map<int, int> baskets;
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < fruits.size(); right++) {
+            baskets[fruits[right]]++;
+            if (baskets.size() > 2) {
+                baskets[fruits[left]]--;
+                if (baskets[fruits[left]] == 0) {
+                    baskets.erase(fruits[left]);
+                }
+                left++;
+            }
+         
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
