a9030636c240a4ae99a96400b4d2e0a5c4f6a650
Abhiram Pedada <149411236+Abhiram-108@users.noreply.github.com>
2025-12-16 10:54:30 +0530

    Time: 133 ms (94.24%) | Memory: 108 MB (47.12%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>&a, int k) {
+        sort(a.begin(),a.end(),greater<int>());
+          int i = 0;
+        long long res = 0;
+
+        while(k--) {
+            a[i] = max(a[i] - i, 0);
+            res += a[i];
+            i++;
+        }
+
+        return res;
+        
+    }
+};
